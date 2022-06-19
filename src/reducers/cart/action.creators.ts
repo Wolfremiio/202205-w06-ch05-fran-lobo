65c85724524c007653e85331bd799b7f3a942366
FranLobo <floboflores@gmail.com>
2022-06-19 18:41:12 +0200

    Making cart and test in reducers

--- src/reducers/cart/action.creators.ts
+++ src/reducers/cart/action.creators.ts
@@ -1,3 +1,25 @@
 import { createAction } from "@reduxjs/toolkit";
-import { Product } from "../../models/product";
+import { cartProduct} from "../../models/cartProduct"
 import { actionTypes } from "../products/action.types";
+
+export interface iAction{
+   type: actionTypes;
+   payload?: any
+}
+
+export const loadProductsAction = createAction<Array<cartProduct>>(
+   actionTypes['cart@load'];
+);
+
+export const addproductAction = createAction<cartProduct>(
+    actionTypes['cart@add'];
+)
+
+export const updateproductAction = createAction<
+cartProduct>(
+    actionTypes['cart@update'];
+)
+
+export const deleteproductAction = createAction<cartProduct>(
+    actionTypes['cart@delete'];
+)
